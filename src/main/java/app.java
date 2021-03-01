453c1780c4ac93beeac878202b5e942b40f48d43
quangnhse05858git <quangnhse05858@fpt.edu.vn>
2021-03-01 02:36:55 -0800

    dm cay

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,6 +1,9 @@
 public class app {
-public static void main(String []args) {
-	System.out.print("hello");
-}
+public static void main(String[] args) throws InterruptedException {
+		while(true) {
+			System.out.print("hello");
+			Thread.sleep(1000L);
+		}
+	}
 }
 
