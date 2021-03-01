89e3d4a59fa1fe372d2903c6ccb20e46923aeee5
quangnhse05858git <quangnhse05858@fpt.edu.vn>
2021-03-01 02:42:26 -0800

    dm cay

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,5 +1,5 @@
 public class app {
-public static void main(String []args) {
+public static void main(String []args) throws InterruptedException{
 	for(int i = 0; i< 15; i++) {
 	System.out.print("hello");
 	Thread.sleep(1000);
